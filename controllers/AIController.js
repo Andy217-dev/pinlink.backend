10588b7628f33ea7cb3952c0949c7003f54a44c6
Andy217-dev <andcandy42@gmail.com>
2025-01-08 22:58:13 -0500

    first commit

--- controllers/AIController.js
+++ controllers/AIController.js
@@ -24,6 +24,16 @@ router.get('/stakingInfo', async (req, res) => {
 	}
 })
 
+router.get('/nonce', async (req, res) => {
+	try {
+		const resp = await axios.get("https://pinlink.ai/api/nonce/"+req.query.address)
+		const { data } = resp;
+		console.log(data)
+		return res.send(data);
+	} catch (e) {
+		console.log(e)
+	}
+})
 // router.post('/', async (req, res) => {
 // 	try {
 // 		return res.send('0:{"a":"$@1","f":"","b":"hMiFO0QGNmXAvekM9nnfN"}\n1:"gpt-4o-mini"');
